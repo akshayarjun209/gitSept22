5b3bbf5099c99ab87dbb894aa999dce55c393246
akshayarjun209 <bhosaleakshay209@gmail.com>
2023-02-02 16:32:03 +0530

    file upload

---
+++ Armstrong.java
@@ -0,0 +1,14 @@
+class Armstrong{
+    public static void main(String args[])
+    {
+        int i=100;
+        int a;
+        while(i<100)
+        {
+            a=0;
+            int n=i;
+            while(n>0)
+        }
+        system.out.println();
+    }
+}
---
+++ Prime.java
@@ -0,0 +1,7 @@
+class Prime{
+    public static void main(String args[])
+    {
+        int a;
+        for(int i=1;i)
+    }
+}
